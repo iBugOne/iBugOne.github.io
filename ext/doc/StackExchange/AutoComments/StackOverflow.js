adb49d2e56ab0df995fe43f10b9aa821932d5012
iBug <7273074+iBug@users.noreply.github.com>
2018-01-03 12:57:36 +0800

    +

--- ext/doc/StackExchange/AutoComments/StackOverflow.js
+++ ext/doc/StackExchange/AutoComments/StackOverflow.js
@@ -2,9 +2,21 @@ callback(
 [
 { "name": "[Q] RegEx Two Problems", "description": "<a href=\"https://blog.codinghorror.com/regular-expressions-now-you-have-two-problems/\">Now you have two problems.</a>"},
 
+{ "name": "[Q] Code as image", "description": "<a href=\"//meta.stackoverflow.com/q/285551/5958455\">Please don't post your code as an image.</a>"},
+
+{ "name": "[Q] \"It doesn't work\" with no exception/error details", "description": "Can you elaborate on how your code \"doesn't work\"? What were you expecting, and what actually happened? If you got an exception/error, post the line it occurred on and the exception/error details. Please [edit] these details in or we may not be able to help."},
+
+{ "name": "[Q] \"Why won't it work?\" questions with no code", "description": "It seems you have a problem with your code. However, we can't help unless we have <a href=\"//$SITEURL$/help/mcve\">code or information that can reproduce the problem</a>. Otherwise, we are just blindly guessing."},
+
+{ "name": "[Q] Homework question without effort", "description": "Questions asking for homework help must include a summary of the work you've done so far to solve the problem, and a description of the difficulty you are having solving it. Please read <a href=\"//meta.stackoverflow.com/q/334822/5958455\">How to ask homework questions</a> and [edit] your post."},
+
 { "name": "[Q] Close > unclear what you're asking", "description": "Please clarify your specific problem or add additional details to highlight exactly what you need. As it's currently written, it’s hard to tell exactly what you're asking. See the [ask] page for help clarifying this question."},
 
-{ "name": "[A] General", "description": "Welcome to $SITENAME$! This is a Q&A site, <a href=\"https://meta.stackexchange.com/q/92107/350567\">not a forum</a>. Please use the `Post Answer` button only if you have a solution to the problem. If you have a <strong>new</strong> question, please ask it by clicking <a href=\"/questions/ask\">Ask Question</a>. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a>, <a href=\"/help/privileges/vote-up\">you can vote up</a> the question. Alternatively, \"star\" it as a favorite and you will be notified of any new answers."},
+{ "name": " Spam!!", "description": "This answer is spam. <a href=\"//meta.stackexchange.com/a/58035/206345\">Flag it as spam</a> by <strong>clicking the `flag` link</strong>!"},
+
+{ "name": "[A] Code only answer", "description": "Thank you for this code snippet, which might provide some limited, immediate help. A <a href=\"//meta.stackexchange.com/q/114762/350567\">proper explanation would greatly improve its long-term value</a> by showing <em>why</em> this is a good solution to the problem, and would make it more useful to future readers with other, similar questions. Please [edit] your answer to add some explanation, including the assumptions you've made."},
+
+{ "name": "[A] General", "description": "This is a Q&A site, <a href=\"https://meta.stackexchange.com/q/92107/350567\">not a forum</a>. Please use the `Post Answer` button only if you have a solution to the problem. If you have a <strong>new</strong> question, please ask it by clicking <a href=\"/questions/ask\">Ask Question</a>. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a>, <a href=\"/help/privileges/vote-up\">you can vote up</a> the question. Alternatively, \"star\" it as a favorite and you will be notified of any new answers."},
 
 { "name": "[A] NAA > 'Thank you' comment", "description": "Please don't add \"thank you\" as an answer. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a>, you will be able to <a href=\"/help/privileges/vote-up\">vote up questions and answers</a> that you found helpful."},
 
@@ -19,6 +31,10 @@ callback(
 { "name": "Expired Link", "description": "<strong>Attention</strong>: This post contains links that are <strong>no longer available</strong>. Please help improve it by replacing with existing, valid links, or text from other sources."},
 
 { "name": "[EQ] Improper tagging", "description": "The tags you were using are not appropriate for this question. Please review <a href=\"/help/tagging\">What are tags, and how should I use them?</a>"},
+
+{ "name": "[EQ] Formatting", "description": "<a href=\"//$SITEURL$/help/formatting\">See how you can format and indent your code correctly</a>"},
+
+{ "name": "[EQ] Hi, and thanks!", "description": "<a href=\"//meta.stackexchange.com/q/2950/350567\">Hi, thanks, ... are considered noise</a>"},
 
 ]
 )
---
+++ ext/doc/StackExchange/AutoComments/StackOverflow.md
@@ -0,0 +1,53 @@
+###[Q] RegEx Two Problems
+[Now you have two problems.](https://blog.codinghorror.com/regular-expressions-now-you-have-two-problems/)
+
+###[Q] Code as image
+[Please don't post your code as an image.](//meta.stackoverflow.com/q/285551/5958455)
+
+###[Q] "It doesn't work" with no exception/error details
+Can you elaborate on how your code "doesn't work"? What were you expecting, and what actually happened? If you got an exception/error, post the line it occurred on and the exception/error details. Please [edit] these details in or we may not be able to help.
+
+###[Q] "Why won't it work?" questions with no code
+It seems you have a problem with your code. However, we can't help unless we have [code or information that can reproduce the problem](//$SITEURL$/help/mcve). Otherwise, we are just blindly guessing.
+
+###[Q] Homework question without effort
+Questions asking for homework help must include a summary of the work you've done so far to solve the problem, and a description of the difficulty you are having solving it. Please read [How to ask homework questions](//meta.stackoverflow.com/q/334822/5958455) and [edit] your post.
+
+###[Q] Close > unclear what you're asking
+Please clarify your specific problem or add additional details to highlight exactly what you need. As it's currently written, it¡¯s hard to tell exactly what you're asking. See the [ask] page for help clarifying this question.
+
+### Spam!!
+This answer is spam. [Flag it as spam](//meta.stackexchange.com/a/58035/206345) by **clicking the `flag` link**!
+
+###[A] Code only answer
+Thank you for this code snippet, which might provide some limited, immediate help. A [proper explanation would greatly improve its long-term value](//meta.stackexchange.com/q/114762/350567) by showing *why* this is a good solution to the problem, and would make it more useful to future readers with other, similar questions. Please [edit] your answer to add some explanation, including the assumptions you've made.
+
+###[A] General
+This is a Q&A site, [not a forum](https://meta.stackexchange.com/q/92107/350567). Please use the `Post Answer` button only if you have a solution to the problem. If you have a **new** question, please ask it by clicking [Ask Question](/questions/ask). Once you have sufficient [reputation](/help/whats-reputation), [you can vote up](/help/privileges/vote-up) the question. Alternatively, "star" it as a favorite and you will be notified of any new answers.
+
+###[A] NAA > 'Thank you' comment
+Please don't add "thank you" as an answer. Once you have sufficient [reputation](/help/whats-reputation), you will be able to [vote up questions and answers](/help/privileges/vote-up) that you found helpful.
+
+###[A] NAA > Link-only answer (and not spam)
+While this link may answer the question, [it is better](//meta.stackexchange.com/q/8231) to include the essential parts of the answer here and provide the link for reference. Link-only answers can become invalid if the linked page changes
+
+###[A] NAA > Commentary on another post, not an answer
+This does not provide an answer to the question. Once you have sufficient [reputation](/help/whats-reputation) you will be able to [comment on any post](/help/privileges/comment); instead, [provide answers that don't require clarification from the asker](//meta.stackexchange.com/q/214173/).
+
+###[A] NAA > Question as an answer
+If you have a new question, please ask it by clicking the [Ask Question](/questions/ask) button. Include a link to this question if it helps provide context.
+
+###[A] NAA > "I'm having this problem, too" comment.
+This does not really answer the question. If you have a different question, you can ask it by clicking [Ask Question](/questions/ask). You can also [add a bounty](/help/privileges/set-bounties) to draw more attention to this question once you have enough [reputation](/help/whats-reputation).
+
+###Expired Link
+**Attention**: This post contains links that are **no longer available**. Please help improve it by replacing with existing, valid links, or text from other sources.
+
+###[EQ] Improper tagging
+The tags you were using are not appropriate for this question. Please review [What are tags, and how should I use them?](/help/tagging)
+
+###[EQ] Formatting
+[See how you can format and indent your code correctly](//$SITEURL$/help/formatting)
+
+###[EQ] Hi, and thanks!
+[Hi, thanks, ... are considered noise](//meta.stackexchange.com/q/2950/350567)
