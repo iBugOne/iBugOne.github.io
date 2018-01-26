3e07674a78e305766e8fd638077973b3136e365c
iBug <7273074+iBug@users.noreply.github.com>
2018-01-26 21:06:51 +0800

    .

--- ext/doc/StackExchange/AutoComments/StackOverflow.js
+++ ext/doc/StackExchange/AutoComments/StackOverflow.js
@@ -2,29 +2,31 @@ callback(
 [
 { "name": "[Q] RegEx Two Problems", "description": "<a href=\"https://blog.codinghorror.com/regular-expressions-now-you-have-two-problems/\">Now you have two problems.</a>"},
 
-{ "name": "[Q] Code as image", "description": "<a href=\"//meta.stackoverflow.com/q/285551/5958455\">Please don't post your code as an image.</a>"},
+{ "name": "[Q] Code as image", "description": "[Please don't post your code as an image.](https://meta.stackoverflow.com/q/285551/5958455)"},
 
 { "name": "[Q] \"It doesn't work\" with no exception/error details", "description": "Can you elaborate on how your code \"doesn't work\"? What were you expecting, and what actually happened? If you got an exception/error, post the line it occurred on and the exception/error details. Please [edit] these details in or we may not be able to help."},
 
 { "name": "[Q] \"Why won't it work?\" questions with no code", "description": "It seems you have a problem with your code. However, we can't help unless we have <a href=\"//$SITEURL$/help/mcve\">code or information that can reproduce the problem</a>. Otherwise, we are just blindly guessing."},
 
-{ "name": "[Q] Homework question without effort", "description": "Questions asking for homework help must include a summary of the work you've done so far to solve the problem, and a description of the difficulty you are having solving it. Please read <a href=\"//meta.stackoverflow.com/q/334822/5958455\">How to ask homework questions</a> and [edit] your post."},
+{ "name": "[Q] Homework question without effort", "description": "Questions asking for homework help must include a summary of the work you've done so far to solve the problem, and a description of the difficulty you are having solving it. Please read [How to ask homework questions](https://meta.stackoverflow.com/q/334822/5958455) and [edit] your post."},
 
 { "name": "[Q] Close > unclear what you're asking", "description": "Please clarify your specific problem or add additional details to highlight exactly what you need. As it's currently written, it’s hard to tell exactly what you're asking. See the [ask] page for help clarifying this question."},
 
-{ "name": "Vandalism", "description": "Please do not vandalize your posts.  By posting on the Stack Exchange network, you've granted a non-revocable right for SE to distribute that content under the [CC BY-SA 3.0 license](https://creativecommons.org/licenses/by-sa/3.0/). By SE policy, any vandalism will be reverted."},
+//{ "name": "Vandalism (SE)", "description": "Please do not vandalize your posts.  By posting on the Stack Exchange network, you've granted a non-revocable right for SE to distribute that content under the [CC BY-SA 3.0 license](https://creativecommons.org/licenses/by-sa/3.0/). By SE policy, any vandalism will be reverted."},
 
-{ "name": "Spam!!", "description": "This answer is spam. <a href=\"//meta.stackexchange.com/a/58035/206345\">Flag it as spam</a> by <strong>clicking the `flag` link</strong>!"},
+{ "name": "Vandalism (SO)", "description": "Please do not vandalize your posts.  By posting on Stack Overflow, you've granted a non-revocable right for SO to distribute that content under the [CC BY-SA 3.0 license](https://creativecommons.org/licenses/by-sa/3.0/). By SO policy, any vandalism will be reverted."},
 
-{ "name": "[A] Code only answer", "description": "Thank you for this code snippet, which might provide some limited, immediate help. A <a href=\"//meta.stackexchange.com/q/114762/350567\">proper explanation would greatly improve its long-term value</a> by showing <em>why</em> this is a good solution to the problem, and would make it more useful to future readers with other, similar questions. Please [edit] your answer to add some explanation, including the assumptions you've made."},
+{ "name": "Spam!!", "description": "This answer is spam. [Flag it as spam](//meta.stackexchange.com/a/58035/206345) by <strong>clicking the `flag` link</strong>!"},
 
-{ "name": "[A] General", "description": "This is a Q&A site, <a href=\"https://meta.stackexchange.com/q/92107/350567\">not a forum</a>. Please use the `Post Answer` button only if you have a solution to the problem. If you have a <strong>new</strong> question, please ask it by clicking <a href=\"/questions/ask\">Ask Question</a>. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a>, <a href=\"/help/privileges/vote-up\">you can vote up</a> the question. Alternatively, \"star\" it as a favorite and you will be notified of any new answers."},
+{ "name": "[A] Code only answer", "description": "Thank you for this code snippet, which might provide some limited, immediate help. A <a href=\"//meta.stackexchange.com/q/114762/350567\">proper explanation would greatly improve its long-term value</a> by showing *why* this is a good solution to the problem, and would make it more useful to future readers with other, similar questions. Please [edit] your answer to add some explanation, including the assumptions you've made."},
+
+// { "name": "[A] General", "description": "This is a Q&A site, <a href=\"https://meta.stackexchange.com/q/92107/350567\">not a forum</a>. Please use the `Post Answer` button only if you have a solution to the problem. If you have a <strong>new</strong> question, please ask it by clicking <a href=\"/questions/ask\">Ask Question</a>. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a>, <a href=\"/help/privileges/vote-up\">you can vote up</a> the question. Alternatively, \"star\" it as a favorite and you will be notified of any new answers."},
 
 { "name": "[A] How not to be a spammer", "description": "Please note if you want to promote your own product, website or blog, <strong>you must disclose your affiliation in the answer</strong>. Otherwise, your answer may be flagged as spam. If you are not affiliated with the site, I recommend saying so to prevent this. Please read <a href=\"https://$SITEURL$/help/promotion\">How to not be a spammer</a>."},
 
 { "name": "[A] NAA > Link-only answer (and not spam)", "description": "While this link may answer the question, <a href=\"//meta.stackexchange.com/q/8231\">it is better</a> to include the essential parts of the answer here and provide the link for reference. Link-only answers can become invalid if the linked page changes"},
 
-{ "name": "[A] NAA > Commentary on another post", "description": "This does not provide an answer to the question. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a> you will be able to <a href=\"/help/privileges/comment\">comment on any post</a>; instead, <a href=\"//meta.stackexchange.com/q/214173/\">provide answers that don't require clarification from the asker</a>."},
+{ "name": "[A] NAA > Commentary on another post", "description": "This does not provide an answer to the question. Once you have sufficient <a href=\"/help/whats-reputation\">reputation</a> you will be able to <a href=\"/help/privileges/comment\">comment on any post</a>; instead, <a href=\"//meta.stackexchange.com/q/214173/350567\">provide answers that don't require clarification from the asker</a>."},
 
 { "name": "[A] NAA > Commentary on another post (enough rep)", "description": "This does not provide an answer to the question. To critique or request clarification from an author, leave a comment below their post."},
 
